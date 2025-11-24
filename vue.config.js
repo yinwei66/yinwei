0d6b0a5acab6e39cc07b2b4ed81db3108afd7ae7
yinwei66 <3159348115@qq.com>
2025-11-24 13:09:03 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: './',
+  devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://124.93.196.45:10001', // 改成你的后端地址
+          changeOrigin: true,
+          pathRewrite: {
+            '^/api': ''  // 去掉/api前缀
+          }
+        }
+      }
+    }
+})
+
